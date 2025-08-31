c929b54f3c21bf55b324aa81fbb6454d1f935bcf
vikas848 <6391782926vs@gmail.com>
2025-08-31 15:12:57 +0530

    data

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+import mysql.connector
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# DB (for login/register)
+auth_db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="vikas",
+    database="qus_data",
+    autocommit=True
+)
+auth_cursor = auth_db.cursor()
+
+@app.route("/")
+def log():
+    return render_template("index.html")
+
+@app.route('/singin')
+def singin():
+    return render_template('singin.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        hashed_password = generate_password_hash(password)
+
+        try:
+            auth_cursor.execute("INSERT INTO qus_data_table (name, email, password) VALUES (%s, %s, %s)",
+                                (username, email, hashed_password))
+            auth_db.commit()
+            session['email'] = email
+            flash("Registration successful!", "success")
+            return redirect(url_for('home'))
+        except mysql.connector.IntegrityError:
+            flash("Email already exists.", "danger")
+
+    return render_template("singin.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':    
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        auth_cursor.execute("SELECT * FROM qus_data_table WHERE email = %s", (email,))
+        user = auth_cursor.fetchone()
+
+        if user:
+            stored_password = user[2]
+            if check_password_hash(stored_password, password):
+                session['email'] = True 
+                return redirect(url_for('home'))
+            else:
+                flash("Invalid password.", "danger")
+        else:
+            flash("Email not found.", "danger")
+
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+   if not session.get('email'):
+        return redirect(url_for('login'))
+   return render_template('home.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    flash("You have been logged out.", "info")
+    return redirect(url_for('log'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5009,host='0.0.0.0')
